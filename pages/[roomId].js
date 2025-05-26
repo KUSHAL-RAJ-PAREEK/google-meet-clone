2fca025c79021f9a491cf623d05fd4bb7033c02e
KUSHAL RAJ PAREEK <115624967+KUSHAL-RAJ-PAREEK@users.noreply.github.com>
2025-05-26 18:07:35 +0530

    stream added

--- pages/[roomId].js
+++ pages/[roomId].js
@@ -1,12 +1,22 @@
 import {useSocket} from "@/context/socket";
 import usePeer from "@/hooks/usePeer"
-import { Inter } from 'next/font/google'
+import useMediaStream from "@/hooks/useMediaStream";
+import {Inter} from 'next/font/google'
 
 import {useEffect} from "react";
+import Player from "@/component/player";
 
-const Room = () =>{
+const Room = () => {
     const socket = useSocket();
-const { peer,myId} = usePeer()
+    const {peer, myId} = usePeer();
+    const {stream} = useMediaStream()
+
+    return (
+    <div>
+ <Player url = {stream} muted playing playerId = {myId}/>
+    </div>
+    )
+
 }
 
 export default Room;
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,13 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        prime: '#212529',
+        secondary: '#343a40',
+        buttonPrimary: '#d90429'
+      }
     },
   },
   plugins: [],
