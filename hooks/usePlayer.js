e31e6d42c69ac08cdfc5275097645c15ac58e4fc
KUSHAL RAJ PAREEK <115624967+KUSHAL-RAJ-PAREEK@users.noreply.github.com>
2025-05-27 07:50:52 +0530

    styling players done

--- hooks/usePlayer.js
+++ hooks/usePlayer.js
@@ -1,9 +1,16 @@
 import {useState} from "react";
+import {cloneDeep} from "lodash";
 
-const usePlayer = () =>{
+const usePlayer = (myId) =>{
  const [players,setPlayers] = useState({})
+    const playersCopy = cloneDeep(players)
 
-    return {players,setPlayers}
+    const playerHighlighted = playersCopy[myId]
+    delete playersCopy[myId]
+
+    const nonHighlightedPlayer = playersCopy
+
+    return {players,setPlayers,playerHighlighted,nonHighlightedPlayer}
 }
 
 export default usePlayer;
--- pages/[roomId].js
+++ pages/[roomId].js
@@ -5,12 +5,12 @@ import {Inter} from 'next/font/google'
 import usePlayer from '@/hooks/usePlayer'
 import {useEffect} from "react";
 import Player from "@/component/player";
-
+import styles from '@/styles/room.module.css'
 const Room = () => {
     const socket = useSocket();
     const {peer, myId} = usePeer();
     const {stream} = useMediaStream()
-    const {players,setPlayers} = usePlayer()
+    const {players,setPlayers,playerHighlighted,nonHighlightedPlayer} = usePlayer(myId)
 
     useEffect(() => {
         if(!socket || !peer || !stream) return;
@@ -71,13 +71,32 @@ const Room = () => {
         }))
     }, [myId,setPlayers,stream]);
     return (
-    <div>
-        {Object.keys(players).map((playerId) =>{
-            const {url,muted,playing} = players[playerId];
-            return  <Player key = {playerId} url = {url} muted={muted} playing = {playing} />
-        })}
-
-    </div>
+    <>
+        <div className={styles.activePlayerContainer}>
+            {playerHighlighted && (
+                <Player
+                    url={playerHighlighted.url}
+                    muted={playerHighlighted.muted}
+                    playing={playerHighlighted.playing}
+                    isActive
+                />
+            )}
+        </div>
+        <div className={styles.inActivePlayerContainer}>
+            {Object.keys(nonHighlightedPlayer).map((playerId) => {
+                const { url, muted, playing } = nonHighlightedPlayer[playerId];
+                return (
+                    <Player
+                        key={playerId}
+                        url={url}
+                        muted={muted}
+                        playing={playing}
+                        isActive={false}
+                    />
+                );
+            })}
+        </div>
+    </>
     )
 
 }
